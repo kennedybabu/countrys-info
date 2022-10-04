aa369dc4995d15e1379492916f072d1ea3215b18
kennedybabu <kennedynjogu8@gmail.com>
2022-10-04 21:02:24 +0300

    feature: get the objects id

--- src/pages/CountryPage.jsx
+++ src/pages/CountryPage.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useParams } from "react-router-dom";
 
-const CountryPage = (prop) => {
+
+const CountryPage = (props) => {
+  const {id} = useParams()
+
+  console.log(id)
+
   return (
     <div>
-        one country
+      <p> this is your id:{id}</p>
     </div>
   )
 }
